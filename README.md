59bf8d4b7009e0de125f3d9467b8388fed89450c
dehp3000 <deh_p3000@yahoo.com>
2020-09-06 14:02:20 -0400

    first upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# <html><center>Week 1</center>
+<br></html>
+
+* YouTube [Video](https://YouTube.com). <-- not yet working just linked to YouTube
+
+* Live [Demo](http://www.gshurstsolutions.com). <-- Not yet implemented to link directly to my website.
+
+* Google [Drive](https://drive.google.com/drive/folders/1QkjtjVLYFcmb1-s4KBpd8IDtCm3er8Ov?usp=sharing)
---
+++ js/blog.js
@@ -0,0 +1,28 @@
+document.querySelector("#blog h2").innerHTML = "Best Parks Myrtle Beach <strong>South Carolina</strong>";
+//use ajax to load JSON file
+var xhr = new XMLHttpRequest();
+xhr.onload = function(){
+	//save JSON data
+	var data = JSON.parse(xhr.responseText);
+	var locationSection = document.querySelector('#blog');
+	if(locationSection){
+
+		var locations = '';
+		for(var i = 0; i < data.locations.length; i++){
+			locations+='<article>';
+			locations+='<p class="thumbnail"><img src ="'+data.locations[i].image+
+			'"alt="'+data.locations[i].location+'"></p>';
+			locations+= '<p>'+data.locations[i].description+'</p>';
+			locations +='<p><button onclick="location.href ='+data.locations[i].more+';">Load More</button></p>';
+			console.log("more = "+data.locations[i].status);
+			//<input type="button" onclick="location.href='https://google.com';" value="Go to Google" />
+			locations+='</article>';
+
+		}
+		locationSection.querySelector('h2').insertAdjacentHTML('afterend',locations);
+	}
+}
+
+//call ajax
+xhr.open('GET','https://dehp3000.github.io/wd3_week1/js/data.JSON',true); 
+xhr.send(null);
