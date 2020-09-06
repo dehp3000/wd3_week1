f922ab2b04d7427d914796f3c315866661d6a25d
dehp3000 <deh_p3000@yahoo.com>
2020-09-06 19:45:14 -0400

    title work

--- js/blog.js
+++ js/blog.js
@@ -13,9 +13,8 @@ if(xhr.status == 200){
 				locations+='<article>';
 				locations+='<p class="thumbnail"><img src ="'+data.locations[i].image+'" alt="'+data.locations[i].location+'"></p>';
 				locations+='<p><h3>'+data.locations[i].location+'</h3></p>';
-				locations+= '<p>'+data.locations[i].description;
+				//locations+= '<p><h4 class ="location">'+data.locations[i].city+'</h4></p>';
 				locations +='<a href="https://southcarolinaparks.com/">More info</a></p>';locations+='</article>';
-
 			}
 			locationSection.querySelector('h2').insertAdjacentHTML('afterend',locations);
 		}
