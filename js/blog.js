640b66e879f92160e2b00c5351475b61d13bcf49
dehp3000 <deh_p3000@yahoo.com>
2020-09-06 18:31:19 -0400

    title work

--- js/blog.js
+++ js/blog.js
@@ -13,7 +13,7 @@ xhr.onload = function(){
 		for(var i = 0; i < data.locations.length; i++){
 			locations+='<article>';
 			console.log(locations[i].location);
-			locations+='<p><h1>'+locations[i].location+'</h></p>';
+			locations+='<p><h3>'+locations[i].location+'</h3></p>';
 			locations+='<p class="thumbnail"><img src ="'+data.locations[i].image+'" alt="'+data.locations[i].location+'"></p>';
 			//console.log(data.locations[i].image);
 			locations+= '<p>'+data.locations[i].description;
