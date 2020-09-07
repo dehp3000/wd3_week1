29356d838c0e4164b2e7ac6861a0d9503e89b063
dehp3000 <deh_p3000@yahoo.com>
2020-09-06 21:48:42 -0400

    test lowercase

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,10 @@
 document.querySelector("#blog h2").innerHTML = "Best Parks Myrtle Beach <strong>South Carolina</strong>";
 //use ajax to load JSON file
 var xhr = new XMLHttpRequest();
-if(xhr.status == 200){
-	xhr.onload = function(){
+
+xhr.onload = function(){
+	console.log(xhr.status);
+	if(xhr.status == 200){
 		//save JSON data
 		var data = JSON.parse(xhr.responseText);
 		var locationSection = document.querySelector('#blog');
@@ -12,15 +14,17 @@ if(xhr.status == 200){
 			for(var i = 0; i < data.locations.length; i++){
 				locations+='<article>';
 				locations+='<p class="thumbnail"><img src ="'+data.locations[i].image+'" alt="'+data.locations[i].location+'"></p>';
-				consol.log(data.locations[i].location);
+				console.log(data.locations[i].location);
 				locations+='<p><h3>'+data.locations[i].location+'</h3></p>';
-				//locations+= '<p><h4 class ="location">'+data.locations[i].city+'</h4></p>';
-				locations +='<a href="https://southcarolinaparks.com/">More info</a></p>';locations+='</article>';
+				//locations+= '<p>'+data.locations[i].city+'</h4></p>';
+				locations +='<a href="https://southcarolinaparks.com/">More info</a></p>';
+				locations+='</article>';
 			}
 			locationSection.querySelector('h2').insertAdjacentHTML('afterend',locations);
 		}
 	};
 };
+
 //call ajax
 xhr.open('GET','https://dehp3000.github.io/wd3_week1/js/data.json',true); 
-xhr.send(null);
+xhr.send();
