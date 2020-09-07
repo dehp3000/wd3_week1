cdac8d04dfe46ffaa48b57383b9fe838e50ad50c
dehp3000 <deh_p3000@yahoo.com>
2020-09-06 22:01:22 -0400

    done

--- js/blog.js
+++ js/blog.js
@@ -14,9 +14,20 @@ xhr.onload = function(){
 			for(var i = 0; i < data.locations.length; i++){
 				locations+='<article>';
 				locations+='<p class="thumbnail"><img src ="'+data.locations[i].image+'" alt="'+data.locations[i].location+'"></p>';
-				console.log(data.locations[i].location);
 				locations+='<p><h3>'+data.locations[i].location+'</h3></p>';
-				//locations+= '<p>'+data.locations[i].city+'</h4></p>';
+				locations +='<p>'+data.locations[i].description+'</p>';
+				locations+= '<p><h4>City: <strong>'+data.locations[i].city+'</strong></h4></p>';
+				locations+='<p><h5>Stars: ';
+				var c = data.locations[i].rating;
+				for ( var b = 5; b >= 0; b--) {
+					if( b <= c){
+						locations+='<span class ="fa fa-star checked"></span>';
+					}else{
+						locations +='<span class ="fa fa-star"></span>';
+					}
+				}
+				locations+=data.locations[i].rating+' out of 5';
+				locations+='<br>Status: '+data.locations[i].status+'</h5></p>';
 				locations +='<a href="https://southcarolinaparks.com/">More info</a></p>';
 				locations+='</article>';
 			}
